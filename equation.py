254eb4dd195de848f85256050f53e3a6dad216d7
mrqler <dangky0407@gmail.com>
2020-03-05 20:48:12 +0700

    add the equation tool

---
+++ equation.py
@@ -0,0 +1,21 @@
+# solve the equation ax^2 + bx + c = 0
+# delta = b^2 - 4ac
+# if delta < 0 No solution
+# if delta = 0
+# if delta > 0
+# input a
+a = finput("please enter a:\n")
+print(f"you have a equal: {a}")
+# input b
+b = input("please enter b:\n")
+print(f"you have b equal: {b}")
+# input c
+c = input("please enter c:\n")
+print(f"you have b equal: {c}")
+# calculate delta:
+delta = b ** - 4 * a * c
+
+# if delta < 0
+if (delta < 0)
+    print("No solutions\n")
+elif
